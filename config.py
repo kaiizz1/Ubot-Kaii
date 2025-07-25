8e664c7ae1a6c880920964587fa5846010af733f
kaiizz1 <manzgames5@gmail.com>
2025-07-25 19:28:23 +0700

    PyroUbot

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+MAX_BOT = int(os.getenv("MAX_BOT", "100"))
+
+DEVS = list(map(int, os.getenv("DEVS", "7753788387").split()))
+
+API_ID = int(os.getenv("API_ID", "27610531"))
+
+API_HASH = os.getenv("API_HASH", "bbdf751d18ccb311d72bd5b87be0ac38")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8363798119:AAFHj-56J4bxFeAGY9j-iKCVCuup4t9Lj0o")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "7753788387"))
+
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285 -1002400165299 -1002416419679 -1001473548283").split()))
+
+RMBG_API = os.getenv("RMBG_API", "MA2sUZ4HdAfBegL36HiG4BUG")
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://fizzpamell:fizzpamell@cluster0.9nmhi5m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002830776134"))
