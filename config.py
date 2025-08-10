b7ff3c5c024d304b0abe13eff12cf72e4338fc7b
kaiizz1 <manzgames5@gmail.com>
2025-08-10 11:10:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-MAX_BOT = int(os.getenv("MAX_BOT", "110"))
+MAX_BOT = int(os.getenv("MAX_BOT", "100"))
 
 DEVS = list(map(int, os.getenv("DEVS", "6699855714").split()))
 
@@ -17,7 +17,7 @@ OWNER_ID = int(os.getenv("OWNER_ID", "6699855714"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285 -1002400165299 -1002416419679 -1001473548283").split()))
 
-RMBG_API = os.getenv("RMBG_API", "MA2sUZ4HdAfBegL36HiG4BUG")
+RMBG_API = os.getenv("RMBG_API", "6rk2bSt7Q2mrdFw9Dv4hTLLS")
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://kaii:6699855714@cluster0.5zgg4u9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
